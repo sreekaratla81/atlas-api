fc721b77d61b3f3d1afc68eb8a65960a748f034a
sreekaratla81 <150553417+sreekaratla81@users.noreply.github.com>
2026-01-04 15:06:36 +0530

    fix build

--- Atlas.Api.IntegrationTests/MigrationGuardTests.cs
+++ Atlas.Api.IntegrationTests/MigrationGuardTests.cs
@@ -2,6 +2,7 @@ using Atlas.Api;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
 using Xunit;
 
 namespace Atlas.Api.IntegrationTests;
